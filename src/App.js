a9441a1ddbc79898b24317c5fe032c974d773475
Siddhartha Chatterjee <ninja.siddhartha@gmail.com>
2022-09-13 18:21:33 -0700

    challenges

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+import axios from "axios";
 
 function App() {
+  const [code, setCode] = useState("");
+  const [passed, setPassed] = useState(0);
+  const tests = [
+    ["5 3", "8"],
+    ["2 4", "6"]
+  ]
+  function submit() { 
+   
+    tests.forEach((test, i) => {
+
+      fetch("http://localhost:3000/submit", {
+        method: "post",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+          code,
+          language: "java",
+          input: test[0],
+        })
+        
+      }).then(response => {
+        console.log(response);
+        return response.json()
+      })
+      .then(data => {
+        console.log(data);
+        if (data.output == test[1]) {
+          setPassed(prev => prev + 1);
+        }
+        else {
+          alert("Failed on test" + i)
+        }
+      })
+    });
+    if (passed == tests.length) {
+      alert("Correct!");
+    }
+  }
+
+  useEffect(() => {
+    if (passed == tests.length) {
+      alert("passed all cases")
+    }
+  }, [passed])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    Given a and b print a + b
+    <textarea value = {code} onChange = {e => setCode(e.target.value)}>
+
+    </textarea>
+    {code}
+    <button onClick={submit}></button>
+    </>
   );
 }
 
